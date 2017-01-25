827967f974718fee4d71a1ba097080a7c2cb47ba
Jo Hund <jhund@clearcove.ca>
2017-01-24 16:35:31 -0800

    Updated version of website widget to current version

--- contentgems.php
+++ contentgems.php
@@ -58,7 +58,7 @@ function createContentGemsWidget($atts, $content = null) {
 
 		$JSEmbed .= "}\n";
 
-    $JSEmbed .= "!function(){function t(){var t=a.createElement(\"script\");t.type=\"text/javascript\",t.async=!0,t.src=\"https://assets.contentgems.com/website-widget/1.0.0/website-widget.js\";var e=a.getElementsByTagName(\"script\")[0];e.parentNode.insertBefore(t,e)}var e=window,a=document;e.attachEvent?e.attachEvent(\"onload\",t):e.addEventListener(\"load\",t,!1)}();";
+    $JSEmbed .= "!function(){function t(){var t=a.createElement(\"script\");t.type=\"text/javascript\",t.async=!0,t.src=\"https://assets.contentgems.com/website-widget/1.1.0/website-widget.js\";var e=a.getElementsByTagName(\"script\")[0];e.parentNode.insertBefore(t,e)}var e=window,a=document;e.attachEvent?e.attachEvent(\"onload\",t):e.addEventListener(\"load\",t,!1)}();";
     $JSEmbed .= "</script>";
 
 		/**
